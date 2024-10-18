9ed78da47597dea7ab91e28570dead9872a0b52d
Kanish027 <mohariyabunty@gmail.com>
2024-10-18 23:56:07 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter, BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./app/Store.js";
+import store from "./app/store.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
